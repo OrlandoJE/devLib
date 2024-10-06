851b09edc085f37efd79946aa3947c5b8042290a
OrlandoJE <orlandoesparza6@gmail.com>
2024-10-06 17:47:49 -0600

    devLib Skeleton

--- src/database.js
+++ src/database.js
@@ -2,6 +2,7 @@ export const Data = {
     categories: {
         "Fonts": [ "item1", "item2", "item3" ],
         "Colors": [ "item4", "item5", "item6" ],
-        "Components": [ "item7", "item8"]
+        "Components": [ "item7", "item8" ],
+        "Componentes": [ "item7"],
     }
 }
